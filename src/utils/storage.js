b8facef83a30dee2508e389c183f019584aeec7b
SibhatY <sabto6659@gmail.com>
2025-04-11 18:50:34 -0500

    Introduce local storage and xp system for user progress and information.

---
+++ src/utils/storage.js
@@ -0,0 +1,23 @@
+const STORAGE_KEY = "playerProgress";
+
+export const loadProgress = () => {
+  const data = localStorage.getItem(STORAGE_KEY);
+  return data ? JSON.parse(data) : { xp: 0, level: 1 };
+};
+
+export const saveProgress = (progress) => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(progress));
+};
+
+export const addXP = (amount) => {
+  const progress = loadProgress();
+  progress.xp += amount;
+
+  while (progress.xp >= progress.level * 100) {
+    progress.xp -= progress.level * 100;
+    progress.level += 1;
+  }
+
+  saveProgress(progress);
+  return progress;
+};
