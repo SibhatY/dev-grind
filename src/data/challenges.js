e208d4f02646c6f414f19515cf84c58d101b1fd8
SibhatY <sabto6659@gmail.com>
2025-04-11 18:46:23 -0500

    Introduce static challenges.

---
+++ src/data/challenges.js
@@ -0,0 +1,21 @@
+const challenges = [
+    {
+      id: 1,
+      prompt: "Write a function that prints 'Hello World'",
+      starterCode: "print('Hello World')",
+      expectedOutput: "Hello World\n",
+      language_id: 71,
+      difficulty: "Easy",
+    },
+    {
+      id: 2,
+      prompt: "Return the reverse of the string 'hello'",
+      starterCode: "print('olleh')",
+      expectedOutput: "olleh\n",
+      language_id: 71,
+      difficulty: "Easy",
+    },
+  ];
+  
+  export default challenges;
+  
