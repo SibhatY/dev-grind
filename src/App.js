7f966f3654ee6aae989ad7d55f345dab822fc053
SibhatY <sabto6659@gmail.com>
2025-04-11 18:41:02 -0500

    Add routing, remove titling and "CodeRunner"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CodeRunner from './components/CodeRunner';
+import PlayerStats from './components/PlayerStats';
+import challenges from './data/challenges';
+import Hub from './pages/Hub';
+import ChallengeList from './pages/ChallengeList';
+import Profile from './pages/Profile';
+import ActiveChallenge from './pages/ActiveChallenge';
 
 function App() {
   return (
-    <div className='main-title'>
-      <h1>Dev Grind</h1>
-      <CodeRunner />
-    </div>
+    <Router>
+      <div className='main-title'>
+        <PlayerStats />
+        <Routes>
+          <Route path='/' element={<Hub />} />
+          <Route path='/challenges' element={<ChallengeList />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/challenge/:id' element={<ActiveChallenge />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
