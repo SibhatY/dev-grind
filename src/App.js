8ca5fb0685bb6986db97128710bdfdc5b56f8c57
SibhatY <sabto6659@gmail.com>
2025-04-16 17:01:22 -0500

    Remove universal stat badge for now. Reroute to include splash page.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CodeRunner from './components/CodeRunner';
-import PlayerStats from './components/PlayerStats';
 import challenges from './data/challenges';
 import Hub from './pages/Hub';
 import ChallengeList from './pages/ChallengeList';
 import Profile from './pages/Profile';
 import ActiveChallenge from './pages/ActiveChallenge';
+import WelcomePage from './pages/WelcomePage';
 
 function App() {
   return (
     <Router>
       <div className='main-title'>
-        <PlayerStats />
         <Routes>
-          <Route path='/' element={<Hub />} />
+          <Route path='/' element={<WelcomePage />} />
+          <Route path='/hub' element={<Hub />} />
           <Route path='/challenges' element={<ChallengeList />} />
           <Route path='/profile' element={<Profile />} />
           <Route path='/challenge/:id' element={<ActiveChallenge />} />
