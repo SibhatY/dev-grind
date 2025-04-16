40679a1a1b9ac3a3a046f7f6fcc1cec274d45974
SibhatY <sabto6659@gmail.com>
2025-04-16 17:02:32 -0500

    Display simple user info for profile screen

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { loadProgress } from '../utils/storage';
+import './Profile.css';
 
 const Profile = () => {
+
+  const { level, xp, rank, rankProgress } = loadProgress();
+
   return (
-    <div>
-      <h2>🧙 Coder Profile</h2>
-      <p>TODO</p>
+    <div className='profile container'>
+      <h2 className='profile-title'>Codex</h2>
+
+      <div className="stat-block">
+        <p><strong>Rank:</strong> {rank}</p>
+        <p><strong>Level:</strong> {level}</p>
+        <p><strong>XP:</strong> {xp}/{level * 100}</p>
+        <p><strong>Rank Trials Completed:</strong> {rankProgress.length}</p>
+      </div>
     </div>
   );
 };
